c61e6ef1d46d70049965ce6345aef1151148f842
Turukmenov <turukmenovv@gmail.com>
2024-02-19 23:59:41 +0600

    homework_3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+export default function App() {
+
+  const message = () => {
+    const inputValue = document.querySelector('.input').value
+    if (inputValue.trim() !== '') {
+      alert('Ваша заметка успешно сохранена')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{width: '1000px', hight: '1000px', display: 'flex', flexWrap: 'wrap', margin: '100px auto', gap: '30px'}}>
+      <div style={{width: '1000px', hight: '300px', backgroundColor: '#b781f3', borderRadius: '5px', color: 'white', textAlign: 'center'}}>
+        <h1>WebSite  TODO</h1>
+      </div>
+      <div style={{width: '1000px', hight: '800px', backgroundColor: '#fff', borderRadius: '5px'}}>
+        <div style={{display: 'flex', flexDirection: 'column', gap: '20px', margin: '30px auto 70px'}}>
+          <div style={{margin: '0 auto'}}>
+            <input className="input" style={{width: '600px', hight: '700px', border: 'none', borderBottom: '3px solid #b6b5be', outline: 'none', fontSize: '36px', color: '#333', marginRight: '30px'}}></input>          
+            <button onClick={message} style={{width: '130px', backgroundColor: '#b781f3',border: 'none', borderRadius: '10px', fontSize: '24px', color: 'white'}}>SUBMIT</button>
+          </div>
+          <div style={{margin: '0 auto'}}>
+            <input className="input" style={{width: '600px', hight: '700px', border: 'none', borderBottom: '3px solid #b6b5be', outline: 'none', fontSize: '36px', color: '#333', marginRight: '30px'}}></input>          
+            <button onClick={message} style={{width: '130px', backgroundColor: '#b781f3',border: 'none', borderRadius: '10px', fontSize: '24px', color: 'white'}}>SUBMIT</button>
+          </div>
+          <div style={{margin: '0 auto'}}>
+            <input className="input" style={{width: '600px', hight: '700px', border: 'none', borderBottom: '3px solid #b6b5be', outline: 'none', fontSize: '36px', color: '#333', marginRight: '30px'}}></input>          
+            <button onClick={message} style={{width: '130px', backgroundColor: '#b781f3',border: 'none', borderRadius: '10px', fontSize: '24px', color: 'white'}}>SUBMIT</button>
+          </div>
+          <div style={{margin: '0 auto'}}>
+            <input className="input" style={{width: '600px', hight: '700px', border: 'none', borderBottom: '3px solid #b6b5be', outline: 'none', fontSize: '36px', color: '#333', marginRight: '30px'}}></input>          
+            <button onClick={message} style={{width: '130px', backgroundColor: '#b781f3',border: 'none', borderRadius: '10px', fontSize: '24px', color: 'white'}}>SUBMIT</button>
+          </div>
+          <div style={{margin: '0 auto'}}>
+            <input className="input" style={{width: '600px', hight: '700px', border: 'none', borderBottom: '3px solid #b6b5be', outline: 'none', fontSize: '36px', color: '#333', marginRight: '30px'}}></input>          
+            <button onClick={message} style={{width: '130px', backgroundColor: '#b781f3',border: 'none', borderRadius: '10px', fontSize: '24px', color: 'white'}}>SUBMIT</button>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
